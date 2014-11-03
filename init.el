cf1383a04c523933d60f932dd5a018fd8c890386
gabrielgrosnyj <gabriel.grosnyj@gmail.com>
2014-11-03 13:30:22 +0000

    move-text fix

--- init.el
+++ init.el
@@ -417,37 +417,43 @@
     (expand-file-name filename)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun move-text-internal (arg)
+  (cond
+   ((and mark-active transient-mark-mode)
+    (if (> (point) (mark))
+        (exchange-point-and-mark))
+    (let ((column (current-column))
+          (text (delete-and-extract-region (point) (mark))))
+      (forward-line arg)
+      (move-to-column column t)
+      (set-mark (point))
+      (insert text)
+      (exchange-point-and-mark)
+      (setq deactivate-mark nil)))
+   (t
+    (let ((column (current-column)))
+      (beginning-of-line)
+      (when (or (> arg 0) (not (bobp)))
+        (forward-line)
+        (when (or (< arg 0) (not (eobp)))
+          (transpose-lines arg))
+        (when (< arg 0)
+          (forward-line -1))
+        (forward-line -1))
+      (move-to-column column t)))))
 
-(defun move-text-internal (arg) 
-   (cond 
-    ((and mark-active transient-mark-mode) 
-     (if (> (point) (mark)) 
-        (exchange-point-and-mark)) 
-     (let ((column (current-column)) 
-          (text (delete-and-extract-region (point) (mark)))) 
-       (forward-line arg) 
-       (move-to-column column t) 
-       (set-mark (point)) 
-       (insert text) 
-       (exchange-point-and-mark) 
-       (setq deactivate-mark nil))) 
-    (t 
-     (beginning-of-line) 
-     (when (or (> arg 0) (not (bobp))) 
-       (forward-line) 
-       (when (or (< arg 0) (not (eobp))) 
-        (transpose-lines arg)) 
-       (forward-line -1))))) 
-(defun move-text-down (arg) 
-   "Move region (transient-mark-mode active) or current line 
-  arg lines down." 
-   (interactive "*p") 
-   (move-text-internal arg)) 
-(defun move-text-up (arg) 
-   "Move region (transient-mark-mode active) or current line 
-  arg lines up." 
-   (interactive "*p") 
-   (move-text-internal (- arg))) 
+(defun move-text-down (arg)
+  "Move region (transient-mark-mode active) or current line
+  arg lines down."
+  (interactive "*p")
+  (move-text-internal arg))
+
+(defun move-text-up (arg)
+  "Move region (transient-mark-mode active) or current line
+  arg lines up."
+  (interactive "*p")
+  (move-text-internal (- arg)))
+
 (global-set-key [(meta shift p)] 'move-text-up) 
 (global-set-key [(meta shift n)] 'move-text-down) 
 
